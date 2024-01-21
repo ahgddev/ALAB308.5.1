55f56b6c4fa99772923b41d90a6534e4fdc2b868
ahgddev <ahgddev@gmail.com>
2024-01-20 20:04:30 -0700

    Finished finding the longest words in an array.

--- lab.js
+++ lab.js
@@ -1,6 +1,7 @@
 // Part 1: Thinking Functionally
 //Take an array of numbers and return the sum
 let nums = [1,2,3,5,7,10,13,14]
+let strs = ["hello","goodbye","good morning","good night","good evening","everyone"]
 
 function addAll(anarray){
     let sum = 0
@@ -17,4 +18,14 @@ function arrayAvg(anarray){
 }
 
 // Take an array of strings and return the longest string.
-function
+function longestStr(anarray) {
+    let wordCheck = []
+    anarray.forEach(word => {
+       wordCheck.unshift(word.length)
+    })
+    let longest = Math.max(...wordCheck)
+    let longWords = anarray.filter((word) => word.length == longest);
+    return longWords
+}
+
+longestStr(strs)
